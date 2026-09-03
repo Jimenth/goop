ec8d5dbf4883fcccb91dd1126096058ec95122c4
region3 <126637834+Jimenth@users.noreply.github.com>
2026-09-03 17:28:24 -0400

    Add files via upload

---
+++ Extra/Offsets/version-e7d81637d42c4b23/Offsets.py
@@ -0,0 +1,791 @@
+# Github: https://git.jonah.cool/jonah/roblox-dumper
+# Roblox Version: version-e7d81637d42c4b23
+# Time Taken: 5584 ms (5.584000 seconds)
+# Total Offsets: 498
+
+class Offsets:
+    ROBLOX_VERSION = "version-e7d81637d42c4b23"
+
+    class AirProperties:
+        AirDensity = 24
+        GlobalWind = 60
+
+    class AnimationTrack:
+        Animation = 184
+        Animator = 272
+        IsPlaying = 2648
+        Looped = 229
+        Speed = 212
+        TimePosition = 216
+
+    class Animator:
+        ActiveAnimations = 2896
+
+    class Atmosphere:
+        Color = 184
+        Decay = 196
+        Density = 208
+        Glare = 212
+        Haze = 216
+        Offset = 220
+
+    class Attachment:
+        Position = 196
+
+    class BasePart:
+        CastShadow = 309
+        Color3 = 424
+        Locked = 310
+        Massless = 311
+        Primitive = 392
+        Reflectance = 268
+        Shape = 440
+        Transparency = 304
+
+    class Beam:
+        Attachment0 = 352
+        Attachment1 = 368
+        Brightness = 384
+        CurveSize0 = 388
+        CurveSize1 = 392
+        LightEmission = 396
+        LightInfluence = 400
+        Texture = 320
+        TextureLength = 412
+        TextureSpeed = 420
+        Width0 = 424
+        Width1 = 428
+        ZOffset = 432
+
+    class BloomEffect:
+        Enabled = 176
+        Intensity = 184
+        Size = 188
+        Threshold = 192
+
+    class BlurEffect:
+        Enabled = 176
+        Size = 184
+
+    class ByteCode:
+        Pointer = 16
+        Size = 40
+
+    class CachedItem:
+        FileMeshData = 64
+
+    class Camera:
+        CFrame = 216
+        CameraSubject = 200
+        CameraType = 312
+        FieldOfView = 320
+        ImagePlaneDepth = 724
+        Position = 252
+        Rotation = 216
+        ViewportInt16 = 652
+        ViewportSize = 716
+
+    class CharacterMesh:
+        BaseTextureId = 200
+        BodyPart = 328
+        MeshId = 248
+        OverlayTextureId = 296
+
+    class ClassDescriptor:
+        ClassName = 8
+        Creator = 560
+        EventDescriptors = 136
+        FunctionDescriptors = 208
+        PropertyDescriptors = 64
+
+    class ClickDetector:
+        MaxActivationDistance = 232
+        MouseIcon = 200
+
+    class Clothing:
+        Color3 = 288
+        Template = 256
+
+    class ColorCorrectionEffect:
+        Brightness = 196
+        Contrast = 200
+        Enabled = 176
+        TintColor = 184
+
+    class ColorGradingEffect:
+        Enabled = 176
+        TonemapperPreset = 184
+
+    class Creator:
+        MapEnd = 138089736
+        MapStart = 138089728
+
+    class DataModel:
+        CreatorId = 384
+        GameId = 392
+        GameLoaded = 1496
+        JobId = 280
+        PlaceId = 400
+        PlaceVersion = 428
+        PrimitiveCount = 1056
+        ScriptContext = 1088
+        ServerIP = 1472
+        ToRenderView1 = 456
+        ToRenderView2 = 8
+        ToRenderView3 = 40
+        Workspace = 344
+
+    class DepthOfFieldEffect:
+        Enabled = 176
+        FarIntensity = 184
+        FocusDistance = 188
+        InFocusRadius = 192
+        NearIntensity = 196
+
+    class Descriptor:
+        Name = 8
+
+    class DragDetector:
+        ActivatedCursorIcon = 448
+        CursorIcon = 200
+        MaxActivationDistance = 232
+        MaxDragAngle = 680
+        MaxDragTranslation = 620
+        MaxForce = 684
+        MaxTorque = 688
+        MinDragAngle = 692
+        MinDragTranslation = 632
+        ReferenceInstance = 496
+        Responsiveness = 704
+
+    class FakeDataModel:
+        Pointer = 147990632
+        RealDataModel = 504
+
+    class FileMeshData:
+        AabbMax = 700
+        AabbMin = 688
+        Faces = 48
+        FacesEnd = 56
+        Vertices = 0
+        VerticesEnd = 8
+
+    class Fire:
+        FireProximityPrompt = 51161424
+
+    class FunctionDescriptor:
+        Function = 128
+
+    class Functions:
+        Clone = 23268496
+        Destroy = 23268528
+        FindPartOnRay = 15463024
+        FindPartOnRayWithIgnoreList = 15463152
+        FindPartOnRayWithWhitelist = 15463296
+        FireServer = 13233872
+        Print = 29925456
+        RaisePropertyChanged = 16168166
+        Raycast = 15427680
+        SetParent = 15403904
+        SetParentInternal = 30223376
+        Shapecast = 15434272
+
+    class GuiBase2D:
+        AbsolutePosition = 264
+        AbsoluteRotation = 232
+        AbsoluteSize = 276
+
+    class GuiObject:
+        Active = 1448
+        AnchorPoint = 1368
+        AutomaticSize = 1376
+        BackgroundColor3 = 1344
+        BackgroundTransparency = 1380
+        BorderColor3 = 1356
+        BorderMode = 1384
+        BorderSizePixel = 1388
+        ClipsDescendants = 1449
+        GuiState = 1400
+        Image = 2440
+        Interactable = 1451
+        LayoutOrder = 1404
+        Position = 1296
+        RichText = 2952
+        Rotation = 232
+        ScreenGui_Enabled = 1220
+        Selectable = 1452
+        SelectionOrder = 1432
+        Size = 1328
+        SizeConstraint = 1440
+        Text = 3568
+        TextColor3 = 3744
+        Visible = 1453
+        ZIndex = 1444
+
+    class Highlight:
+        Adornee = 184
+        DepthMode = 224
+        Enabled = 244
+        FillColor = 200
+        FillTransparency = 228
+        OutlineColor = 212
+        OutlineTransparency = 236
+
+    class HopperBin:
+        BinType = 1128
+
+    class Humanoid:
+        AutoJumpEnabled = 468
+        AutoRotate = 469
+        AutomaticScalingEnabled = 470
+        BreakJointsOnDeath = 471
+        CameraOffset = 296
+        DisplayDistanceType = 384
+        DisplayName = 184
+        EvaluateStateMachine = 472
+        FloorMaterial = 388
+        Health = 400
+        HealthDisplayDistance = 392
+        HealthDisplayType = 396
+        HipHeight = 404
+        HumanoidRootPart = 1144
+        HumanoidState = 2240
+        HumanoidStateID = 32
+        IsWalking = 2407
+        Jump = 474
+        JumpHeight = 416
+        JumpPower = 420
+        MaxHealth = 424
+        MaxSlopeAngle = 428
+        MoveDirection = 320
+        MoveToPart = 280
+        NameDisplayDistance = 432
+        NameOcclusion = 436
+        PlatformStand = 476
+        PlatformStatePointer = 0
+        RequiresNeck = 477
+        RigType = 448
+        SeatPart = 264
+        Sit = 478
+        TargetPoint = 332
+        UseJumpPower = 480
+        WalkSpeed = 464
+        WalkSpeedCheck = 956
+        WalkTimer = 1032
+        WalkToPoint = 356
+
+    class ICreator:
+        Create = 0
+
+    class InputObject:
+        MousePosition = 212
+
+    class Instance:
+        ChildrenEnd = 8
+        ChildrenStart = 120
+        ClassBase = 432
+        ClassDescriptor = 24
+        Name = 8
+        NameContainer = 112
+        Parent = 104
+
+    class Lighting:
+        Ambient = 208
+        Atmosphere = 472
+        Brightness = 280
+        ClockTime = 200
+        ColorShift_Bottom = 220
+        ColorShift_Top = 232
+        EnvironmentDiffuseScale = 284
+        EnvironmentSpecularScale = 288
+        ExposureCompensation = 292
+        FogColor = 244
+        FogEnd = 300
+        FogStart = 304
+        GeographicLatitude = 308
+        GlobalShadows = 324
+        GradientBottom = 400
+        GradientTop = 336
+        LightColor = 348
+        LightDirection = 360
+        MoonPosition = 388
+        OutdoorAmbient = 256
+        ShadowSoftness = 316
+        Sky = 456
+        Source = 372
+        SunPosition = 376
+
+    class LightingParameters:  # these are in the lighting service
+        GeographicLatitude = 308
+        LightColor = 348
+        LightDirection = 360
+        SkyAmbient = 336
+        SkyAmbient2 = 312
+        Source = 372
+        TrueMoonPosition = 388
+        TrueSunPosition = 376
+
+    class LocalScript:
+        ByteCode = 0
+        Bytecode = 400
+        Hash = 208
+
+    class LruHolder:
+        MemEnforcedLRUCache = 32
+
+    class LruNode:
+        CachedItem = 64
+        MeshId = 16
+        Next = 0
+
+    class MaterialColors:
+        Asphalt = 48
+        Basalt = 39
+        Brick = 15
+        Cobblestone = 51
+        Concrete = 12
+        CrackedLava = 45
+        Glacier = 27
+        Grass = 6
+        Ground = 42
+        Ice = 54
+        LeafyGrass = 57
+        Limestone = 63
+        Mud = 36
+        Pavement = 66
+        Rock = 24
+        Salt = 60
+        Sand = 18
+        Sandstone = 33
+        Slate = 9
+        Snow = 30
+        WoodPlanks = 21
+
+    class MemEnforcedLRUCache:
+        Head = 8
+
+    class MeshContentProvider:
+        LruHolder = 216
+
+    class MeshData:
+        FaceEnd = 56
+        FaceStart = 48
+        VertexEnd = 8
+        VertexStart = 0
+
+    class MeshPart:
+        MeshId = 784
+        TextureId = 832
+
+    class Misc:
+        Adornee = 240
+        AnimationId = 192
+        StringLength = 16
+        Value = 184
+
+    class Model:
+        PrimaryPart = 600
+        Scale = 324
+
+    class ModuleScript:
+        ByteCode = 0
+        Bytecode = 312
+        Hash = 208
+        IsRobloxScript = 360
+
+    class MouseService:
+        InputObject = 256
+        MousePosition = 212
+        SensitivityPointer = 0
+
+    class ParticleEmitter:
+        Acceleration = 480
+        Brightness = 540
+        Drag = 544
+        Lifetime = 500
+        LightEmission = 568
+        LightInfluence = 572
+        Rate = 584
+        RotSpeed = 508
+        Rotation = 516
+        Speed = 524
+        SpreadAngle = 532
+        Texture = 448
+        TimeScale = 604
+        VelocityInheritance = 608
+        ZOffset = 612
+
+    class Player:
+        AccountAge = 860
+        CameraMode = 880
+        Character = 664
+        DisplayName = 312
+        HealthDisplayDistance = 916
+        LocalPlayer = 304
+        LocaleId = 1864
+        MaxZoomDistance = 872
+        MinZoomDistance = 876
+        Mouse = 4616
+        NameDisplayDistance = 932
+        Team = 728
+        TeamColor = 944
+        UserId = 208
+
+    class PlayerConfigurer:
+        Pointer = 0
+
+    class PlayerMouse:
+        Icon = 200
+        Workspace = 336
+
+    class Players:
+        LocalPlayer = 304
+
+    class Primitive:
+        AssemblyAngularVelocity = 260
+        AssemblyLinearVelocity = 248
+        CFrame = 200
+        Material = 582
+        Orientation = 200
+        Part = 528
+        Position = 236
+        PrimitiveFlags = 438
+        Rotation = 200
+        Size = 444
+        Validate = 6
+
+    class PrimitiveFlags:
+        Anchored = 2
+        CanCollide = 8
+        CanQuery = 32
+        CanTouch = 16
+
+    class PropertyDescriptor:
+        GetSetImpl = 144
+        TType = 104
+
+    class ProximityPrompt:
+        ActionText = 176
+        Enabled = 310
+        GamepadKeyCode = 284
+        HoldDuration = 288
+        KeyboardKeyCode = 292
+        MaxActivationDistance = 296
+        ObjectText = 208
+        RequiresLineOfSight = 311
+
+    class RenderJob:
+        FakeDataModel = 56
+        RealDataModel = 496
+        RenderView = 472
+
+    class RenderView:
+        DeviceD3D11 = 8
+        LightingValid = 552
+        SkyboxValid = 653
+        VisualEngine = 16
+
+    class RunService:
+        HeartbeatFPS = 192
+        HeartbeatTask = 224
+
+    class Script:
+        ByteCode = 0
+        GUID = 208
+        Hash = 416
+
+    class ScriptContext:
+        RequireBypass = 2817
+
+    class Seat:
+        Occupant = 536
+
+    class Sky:
+        MoonAngularSize = 580
+        MoonTextureId = 200
+        SkyboxBk = 248
+        SkyboxDn = 296
+        SkyboxFt = 344
+        SkyboxLf = 392
+        SkyboxOrientation = 568
+        SkyboxRt = 440
+        SkyboxUp = 488
+        StarCount = 584
+        SunAngularSize = 588
+        SunTextureId = 536
+
+    class Sound:
+        IsPlaying = 320
+        Looped = 317
+        PlaybackSpeed = 284
+        RollOffMaxDistance = 288
+        RollOffMinDistance = 292
+        SoundGroup = 232
+        SoundId = 200
+        Volume = 304
+
+    class SpawnLocation:
+        AllowTeamChangeOnTouch = 61
+        Enabled = 497
+        ForcefieldDuration = 488
+        Neutral = 498
+        TeamColor = 492
+
+    class SpecialMesh:
+        MeshId = 248
+        Offset = 184
+        Scale = 196
+        TextureId = 296
+
+    class StatsItem:
+        Value = 200
+
+    class SunRaysEffect:
+        Enabled = 176
+        Intensity = 184
+        Spread = 188
+
+    class SurfaceAppearance:
+        AlphaMode = 496
+        Color = 472
+        ColorMap = 200
+        EmissiveMaskContent = 248
+        EmissiveStrength = 500
+        EmissiveTint = 484
+        MetalnessMap = 296
+        NormalMap = 344
+        RoughnessMap = 392
+
+    class TaskScheduler:
+        JobEnd = 208
+        JobName = 24
+        JobStart = 200
+        MaxFPS = 176
+        Pointer = 145479464
+
+    class Team:
+        TeamColor = 184
+
+    class Terrain:
+        GrassLength = 496
+        MaterialColors = 1208
+        WaterColor = 480
+        WaterReflectance = 504
+        WaterTransparency = 508
+        WaterWaveSize = 512
+        WaterWaveSpeed = 516
+
+    class TextButton:
+        AutoButtonColor = 2500
+        ContentText = 3592
+        LineHeight = 3872
+        LocalizedText = 3592
+        MaxVisibleGraphemes = 4412
+        Modal = 2501
+        RichText = 4126
+        Selected = 2502
+        Text = 3592
+        TextColor3 = 4384
+        TextDirection = 4032
+        TextScaled = 3569
+        TextSize = 4420
+        TextStrokeColor3 = 4396
+        TextStrokeTransparency = 4424
+        TextTransparency = 4428
+        TextTruncate = 4432
+        TextWrapped = 4120
+        TextXAlignment = 4436
+        TextYAlignment = 3944
+
+    class TextLabel:
+        ContentText = 2952
+        LineHeight = 3232
+        LocalizedText = 2952
+        MaxVisibleGraphemes = 3772
+        RichText = 3486
+        Text = 2952
+        TextColor3 = 3744
+        TextDirection = 3392
+        TextScaled = 2929
+        TextSize = 3780
+        TextStrokeColor3 = 3756
+        TextStrokeTransparency = 3784
+        TextTransparency = 3788
+        TextTruncate = 3481
+        TextWrapped = 3480
+        TextXAlignment = 3796
+        TextYAlignment = 3304
+
+    class Textures:
+        Decal_Texture = 432
+
+    class Tool:
+        CanBeDropped = 1208
+        Enabled = 1209
+        Grip = 1160
+        GripForward = 1184
+        GripPos = 1196
+        GripRight = 1160
+        GripUp = 1172
+        ManualActivationOnly = 1210
+        RequiresHandle = 1211
+        TextureId = 864
+        Tooltip = 1128
+
+    class Types:
+        AllTypes = 143297672
+
+    class UnionOperation:
+        AssetId = 784
+
+    class UserInputService:
+        WindowInputState = 704
+
+    class Value:
+        Value = 184
+
+    class VehicleSeat:
+        MaxSpeed = 552
+        Occupant = 520
+        SteerFloat = 556
+        ThrottleFloat = 560
+        Torque = 564
+        TurnSpeed = 568
+
+    class VisualEngine:
+        Dimensions = 2832
+        FakeDataModel = 2800
+        Pointer = 137696264
+        RenderView = 3120
+        ViewMatrix = 432
+
+    class Weld:
+        Part0 = 280
+        Part1 = 296
+
+    class WeldConstraint:
+        Part0 = 184
+        Part1 = 200
+
+    class WindowInputState:
+        CapsLock = 64
+        CurrentTextBox = 72
+
+    class Workspace:
+        CurrentCamera = 1208
+        DistributedGameTime = 1240
+        ReadOnlyGravity = 2544
+        World = 1040
+
+    class World:
+        AirProperties = 576
+        FallenPartsDestroyHeight = 544
+        Gravity = 552
+        Primitives = 688
+        WorldSteps = 1832
+
+
+    class ReflectionType:
+        Null = 0
+        Bool = 1
+        Int = 2
+        Int64 = 3
+        Float = 4
+        Double = 5
+        String = 6
+        ProtectedString = 7
+        Instance = 8
+        Instances = 9
+        Ray = 10
+        Vector2 = 11
+        Vector3 = 12
+        Vector2int16 = 13
+        Vector3int16 = 14
+        Rect2D = 15
+        CoordinateFrame = 16
+        Color3 = 17
+        Color3uint8 = 18
+        UDim = 19
+        UDim2 = 20
+        Faces = 21
+        Axes = 22
+        Region3 = 23
+        Region3int16 = 24
+        CellId = 25
+        GuidData = 26
+        PhysicalProperties = 27
+        BrickColor = 28
+        SystemAddress = 29
+        BinaryString = 30
+        Surface = 31
+        CollectionHandle = 32
+        Enum = 33
+        Property = 34
+        Tuple = 35
+        Array = 36
+        Dictionary = 37
+        Map = 38
+        Variant = 39
+        GenericFunction = 40
+        Function = 41
+        ColorSequence = 42
+        ColorSequenceKeypoint = 43
+        NumberRange = 44
+        NumberSequence = 45
+        NumberSequenceKeypoint = 46
+        Connection = 48
+        ContentId = 49
+        DescribedBase = 50
+        RefType = 51
+        EventInstance = 54
+        TweenInfo = 55
+        DockWidgetPluginGuiInfo = 56
+        PluginDrag = 57
+        Random = 58
+        PathWaypoint = 59
+        FloatCurveKey = 60
+        RotationCurveKey = 61
+        ValueCurveKey = 62
+        SharedString = 63
+        DateTime = 64
+        RaycastParams = 65
+        RaycastResult = 66
+        OverlapParams = 67
+        LazyTable = 68
+        DebugTable = 69
+        CatalogSearchParams = 70
+        OptionalCoordinateFrame = 71
+        CSGPropertyData = 72
+        UniqueId = 73
+        Font = 74
+        SharedTable = 75
+        SharedTableIterator = 76
+        AnimationMask = 77
+        AnimationPose = 78
+        ClipEvaluator = 79
+        OpenCloudModel = 80
+        InstanceRef = 81
+        SecurityCapabilities = 82
+        ArticulatedJoint = 83
+        AnimationContext = 84
+        Secret = 85
+        Buffer = 86
+        Integer = 87
+        Path2DControlPoint = 88
+        ReplicationPV = 89
+        FacsReplicationData = 90
+        AnimationMaskModifier = 91
+        Content = 92
+        NetAssetHandle = 93
+        NetAssetRef = 94
+        Object = 95
+        AdReward = 96
+        AssetContentMap = 97
+        SlimReplicationData = 98
+        User = 99
+        WebViewParams = 100
+        AnimTrackPlayState = 101
+        AnimTrackMetadata = 102
+        AnimTrackWeight = 103
+        ScopedInstanceIdentity = 104
+
